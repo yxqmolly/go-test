06fd32678e1873c18587184178c96d046de165ac
wb_yanxinqiang <wb_yanxinqiang@meituan.com>
2025-04-08 00:42:40 +0800

    testMap

--- src/base/map/mapTest.go
+++ src/base/map/mapTest.go
@@ -1,12 +1,26 @@
+package main
+
+import "fmt"
 
 func modifyUser(users map[string]map[string]string, name string){
 	if users[name] != nil {
 		users[name]["pwd"] = "12121"
 	}else {
-		users[name] = map(map[string])
+		users[name] = make(map[string]string, 2)
+		users[name]["name"] = name
+		users[name]["pwd"] = "123456"
 	}
 }
 
-func ()  {
+func main() {
+	users := make(map[string]map[string]string, 10)
+	users["smith"] = map[string]string{
+		"name": "smith",
+		"pwd": "123456",
+	}
+	modifyUser(users, "nora")
+	for key,val := range users{
+		fmt.Println("key= ", key, "val=", val)
+	}
 	
 }
