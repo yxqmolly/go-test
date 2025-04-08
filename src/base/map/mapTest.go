a1edbb9ff8680f5a148d66c87f4a04a5f1b6e99c
wb_yanxinqiang <wb_yanxinqiang@meituan.com>
2025-04-09 00:47:37 +0800

    map练习

--- src/base/map/mapTest.go
+++ src/base/map/mapTest.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-func modifyUser(users map[string]map[string]string, name string){
+func modifyUser(users map[string]map[string]string, name string) {
 	if users[name] != nil {
 		users[name]["pwd"] = "12121"
-	}else {
+	} else {
 		users[name] = make(map[string]string, 2)
 		users[name]["name"] = name
 		users[name]["pwd"] = "123456"
@@ -13,14 +13,55 @@ func modifyUser(users map[string]map[string]string, name string){
 }
 
 func main() {
+	test3()
+
+}
+
+/**
+ *map的value是map类型时，不能使用make创建，必须使用make(map[string]map[string]string)
+ */
+func test1() {
 	users := make(map[string]map[string]string, 10)
 	users["smith"] = map[string]string{
 		"name": "smith",
-		"pwd": "123456",
+		"pwd":  "123456",
 	}
 	modifyUser(users, "nora")
-	for key,val := range users{
+	for key, val := range users {
 		fmt.Println("key= ", key, "val=", val)
 	}
-	
+}
+/** 
+ *map的增删改查
+ */
+func test2() {
+	//增加
+	cites := make(map[string]string)
+	cites["address1"] = "beijing"
+	cites["address2"] = "shanghai"
+	cites["address3"] = "shenzhen"
+	fmt.Println(cites)
+	//修改
+	cites["address1"] = "ningde"
+	fmt.Println(cites)
+	//判断是否存在
+	if val ,exists:= cites["address11"]; exists{
+		fmt.Printf("beijing:%s", val)
+	}
+	//删除
+	delete(cites, "address21")
+	cites =make(map[string]string)
+	fmt.Println(cites)
+}
+func test3(){
+	var testMap map[string]string
+	testMap = make(map[string]string,10)
+	fmt.Println(testMap)
+	for i:=0; i<120; i++{
+		key := fmt.Sprintf("stu%d", i)
+		value := fmt.Sprintf("stu%d", i)
+		testMap[key] = value
+	}
+	fmt.Println(testMap)
+	fmt.Println("testMap的长度为：%d", len(testMap))
 }
